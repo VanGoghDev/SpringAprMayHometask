993559e2657201907d62ab2a07104d16010ef05d
VanGoghDev <kifirigor@gmail.com>
2019-05-02 21:47:23 +0300

    Added hospital-customer dependencies (using xml, JavaConfig. annotations)

--- src/main/java/ru/firsov/hometask1/HospitalImpl.java
+++ src/main/java/ru/firsov/hometask1/HospitalImpl.java
@@ -1,6 +1,14 @@
 package ru.firsov.hometask1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("hospital")
 public class HospitalImpl implements IHospital{
+
+    @Autowired(required = false)
+    @Qualifier("adultCustomer")
     private ICustomer customer;
 
     public void sayCabinet() {
@@ -9,7 +17,7 @@ public class HospitalImpl implements IHospital{
         else if (customer.getAge() >= 19 && customer.getDisease().equals("cough"))
             System.out.println("Cabinet of therapist number 2");
         else if (customer.getAge() >= 19 && customer.getDisease().equals("gastritis"))
-            System.out.println("Cabinet of therapist number 3");
+            System.out.println("Cabinet of gastroenterologist number 3");
     }
 
     public void setCustomer(ICustomer customer) {
